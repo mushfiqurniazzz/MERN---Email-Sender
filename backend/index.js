ccc89d950da3559b3ec7976969e986f55e22abe7
Mushfiqur Niaz <mushfiqurniaz33.com>
2024-04-22 18:37:49 +0600

    notes in backend

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+//importing the neccessary functions, libraries and environmental variables to make app run
 const express = require('express')
 const app = express()
 const cors = require("cors")
@@ -5,13 +6,17 @@ const {sendEmailController, getUsers } = require("./controllers/emailController"
 const ConDB = require("./DB/ConDB.js")
 const port = process.env.PORT || 3000
 require("dotenv").config()
-
 app.use(cors())
 app.use(express.json())
-app.post("/", sendEmailController)
-app.get("/", getUsers)
 
+//we declared the routes in the index.js as theres only one route but we will be sending two type of requests on the same route
+app.post("/", sendEmailController) //this will send the email
+app.get("/", getUsers) //this will send the number of users in the env
+
+//the mongodb connection function
 ConDB()
+
+//making the app listen on a specified port and then console.log
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
